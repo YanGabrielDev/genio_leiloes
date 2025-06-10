99fd66728b1eb731b3d273f7882ce1a892f823d0
Yan_Gabriel <yangabriel2012@gmail.com>
2025-06-09 22:22:33 -0400

    trocando no mobile

--- src/features/MyAccount/components/AccountCard/index.tsx
+++ src/features/MyAccount/components/AccountCard/index.tsx
@@ -1,36 +1,29 @@
 import { motion } from 'framer-motion'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { ReactNode } from 'react'
-import { cn } from '@/lib/utils'
 
-export interface AccountCardProps {
+interface AccountCardProps {
   title: string
   value: string
-  icon: ReactNode
-  className?: string
+  icon: React.ReactNode
 }
 
 export const AccountCard = ({ title, value, icon }: AccountCardProps) => {
   return (
     <motion.div
-      whileHover={{ y: -5 }} // Remove animação no modo compacto
+      whileHover={{ y: -3 }}
       transition={{ duration: 0.2 }}
       className="h-full"
     >
-      <Card className={cn('h-full transition-all', 'hover:shadow-md')}>
-        <CardHeader
-          className={cn(
-            'flex flex-row items-center justify-between pb-2 space-y-0'
-          )}
-        >
-          <CardTitle className={cn('font-medium text-gray-500')}>
+      <Card className="h-full transition-all hover:shadow-sm md:hover:shadow">
+        <CardHeader className="flex flex-row items-center justify-between pb-1 space-y-0 md:pb-2">
+          <CardTitle className="text-xs font-medium text-gray-500 sm:text-sm md:text-base">
             {title}
           </CardTitle>
-          <div className={''}>{icon}</div>
+          <div className="scale-75 sm:scale-90 md:scale-100">{icon}</div>
         </CardHeader>
-        <CardContent className={''}>
+        <CardContent className="p-2 sm:p-3 md:p-4">
           <div
-            className={cn('font-bold', 'text-2xl', 'truncate')}
+            className="text-sm font-bold sm:text-base md:text-lg lg:text-xl truncate"
             title={value}
           >
             {value}
