6e19262d5533eb0ef37cbd30bac90ab3e9133dcc
Yan_Gabriel <yangabriel2012@gmail.com>
2025-06-11 23:31:50 -0400

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  base: '/',
+
   plugins: [
     react(),
     TanStackRouterVite({
       routesDirectory: path.resolve(__dirname, './src/routes'),
       generatedRouteTree: path.resolve(__dirname, './src/routeTree.gen.ts'),
-    })
+    }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   optimizeDeps: {
-    include: [
-      '@tanstack/react-router',
-      '@tanstack/router-plugin/vite'
-    ]
-  }
+    include: ['@tanstack/react-router', '@tanstack/router-plugin/vite'],
+  },
 })
