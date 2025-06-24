3ab25a78e6d549cad47a05bed2ca161965ce3dba
Yan_Gabriel <yangabriel2012@gmail.com>
2025-06-23 22:30:50 -0400

    handle

--- src/features/payment/components/PaymentForm/index.tsx
+++ src/features/payment/components/PaymentForm/index.tsx
@@ -1,13 +1,9 @@
 import { useState, FormEvent } from 'react'
-import {
-  useStripe,
-  // useElements,
-  //  CardElement
-} from '@stripe/react-stripe-js'
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js'
 
 export function PaymentForm() {
   const stripe = useStripe()
-  //   const elements = useElements()
+  const elements = useElements()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
   const [success, setSuccess] = useState(false)
@@ -17,11 +13,11 @@ export function PaymentForm() {
     setLoading(true)
     setError(null)
 
-    // if (!stripe || !elements) {
-    //   setError('Stripe ainda não carregou.')
-    //   setLoading(false)
-    //   return
-    // }
+    if (!stripe || !elements) {
+      setError('Stripe ainda não carregou.')
+      setLoading(false)
+      return
+    }
 
     try {
       const res = await fetch('http://localhost:3001/create-payment-intent', {
@@ -32,17 +28,17 @@ export function PaymentForm() {
 
       const { clientSecret } = await res.json()
 
-      //   const result = await stripe.confirmCardPayment(clientSecret, {
-      //     payment_method: {
-      //       card: elements.getElement(CardElement)!,
-      //     },
-      //   })
+      const result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement)!,
+        },
+      })
 
-      //   if (result.error) {
-      //     setError(result.error.message || 'Erro ao processar pagamento.')
-      //   } else if (result.paymentIntent?.status === 'succeeded') {
-      //     setSuccess(true)
-      //   }
+      if (result.error) {
+        setError(result.error.message || 'Erro ao processar pagamento.')
+      } else if (result.paymentIntent?.status === 'succeeded') {
+        setSuccess(true)
+      }
     } catch (err) {
       setError('Erro inesperado.')
     } finally {
@@ -57,11 +53,11 @@ export function PaymentForm() {
       onSubmit={handleSubmit}
       style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
     >
-      {/* <CardElement options={{ hidePostalCode: true }} />
+      <CardElement options={{ hidePostalCode: true }} />
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <button type="submit" disabled={!stripe || loading}>
         {loading ? 'Processando...' : 'Pagar'}
-      </button> */}
+      </button>
     </form>
   )
 }
