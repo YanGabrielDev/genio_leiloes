e50962a675906f010837f1a45422555a33a8ba14
Yan_Gabriel <yangabriel2012@gmail.com>
2025-07-14 23:15:12 -0400

    fix seo

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import Sitemap from 'vite-plugin-sitemap'
+
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
+    Sitemap({ hostname: 'https://genio-leiloes.vercel.app' }),
+
     TanStackRouterVite({
       routesDirectory: path.resolve(__dirname, './src/routes'),
       generatedRouteTree: path.resolve(__dirname, './src/routeTree.gen.ts'),
