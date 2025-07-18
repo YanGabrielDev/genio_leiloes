4e969fce85f37876b44ba63b5c5731beabeb4441
Yan_Gabriel <yangabriel2012@gmail.com>
2025-07-18 00:05:28 -0400

    fix hero colors

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -11,7 +11,7 @@ function LoginPage() {
       <div className="relative col-span-12 md:col-span-4 h-screen bg-white flex justify-center items-center p-8">
         <LoginForm />
       </div>
-      <div className="hidden md:block col-span-8 h-screen bg-[#fffaea] p-8">
+      <div className="hidden md:block col-span-8 h-screen bg-primary p-8">
         <img
           src={hero}
           alt="um matelo e um carro de leilao"
