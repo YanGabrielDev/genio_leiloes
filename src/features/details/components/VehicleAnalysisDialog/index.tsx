4c22c6dd797aa9ed40ccb9d2ca178b4b116a358b
Yan_Gabriel <yangabriel2012@gmail.com>
2025-07-17 23:58:58 -0400

    fix build

--- src/features/details/components/VehicleAnalysisDialog/index.tsx
+++ src/features/details/components/VehicleAnalysisDialog/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { AnimatePresence, motion } from 'framer-motion'
 import { Loader2 } from 'lucide-react'
 import { useAnalysis } from '../../hooks/useAnalysis'
