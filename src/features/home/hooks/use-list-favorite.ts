6392d9f859382448934bb631a5aad4365771b4e3
Yan_Gabriel <yangabriel2012@gmail.com>
2025-07-22 23:20:24 -0400

    fix fav

--- src/features/home/hooks/use-list-favorite.ts
+++ src/features/home/hooks/use-list-favorite.ts
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
 import auctionService from '../services/auction/auction.service'
-
+import Cookies from 'js-cookie'
 export const useListFavorite = () => {
+  const cookies = Cookies
+  const token = cookies.get('accessToken')
+
   return useQuery({
     queryKey: ['list-favorite'],
     queryFn: () => auctionService.listFavorite(),
     staleTime: 1000 * 60 * 5,
+    enabled: !!token,
   })
 }
