c648e6df62c94837feffdc9860b0df9dd72c40ba
Yan_Gabriel <yangabriel2012@gmail.com>
2025-09-18 21:44:31 -0400

    add sm:header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -157,7 +157,7 @@ export const Header = ({
                   </div>
                 )}
 
-                <div className="relative flex gap-4 items-center w-full flex-col sm:flex-row">
+                <div className="relative flex gap-4 sm:items-center w-full flex-col sm:flex-row">
                   <div className=" flex gap-4 items-start">
                     {showFilters && (
                       <VehicleFilters cityFilterOptions={cityFilterOptions} />
