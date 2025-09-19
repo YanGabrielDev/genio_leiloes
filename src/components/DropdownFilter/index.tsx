2e4c1972c24fce0671c3a9431c62b7311960f920
Yan_Gabriel <yangabriel2012@gmail.com>
2025-09-18 23:17:09 -0400

    corrigindo filtro header

--- src/components/DropdownFilter/index.tsx
+++ src/components/DropdownFilter/index.tsx
@@ -67,7 +67,7 @@ export function DropdownFilter({
     return (
       <div
         style={style}
-        className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer transition-colors"
+        className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer transition-colors whitespace-nowrap"
         onClick={() => handleSelect(option)}
       >
         {option.label}
