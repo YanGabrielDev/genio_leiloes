110a1fce8bc8dec968c1f0288f07cf0115fe5be3
Yan_Gabriel <yangabriel2012@gmail.com>
2025-11-06 21:35:42 -0500

    fix: fixed grid cols

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -103,7 +103,7 @@ function SearchPage() {
           Veículos Disponíveis:
         </h2>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-12 gap-4">
         {listAuction.isLoading ? (
           <SkeletonLoaderGrid count={12} />
         ) : (
